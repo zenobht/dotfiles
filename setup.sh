cd3b5d8c3d48e875489942fc729efbdf23bd5f2f
Bharat Joshi <bharatjoshi.p@gmail.com>
2019-12-29 16:21:50 +1100

    fix shebang error

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-!/bin/zsh
+#!/bin/zsh
 
 # install brew packages ---------------------------------------------------------------------------------------------------------------------------------------------------------
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
