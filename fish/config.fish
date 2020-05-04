9927285b8e9aa7aab3298bee225b250a3843245d
bharat <bharatjoshi.p@gmail.com>
2020-05-04 22:11:56 +1000

    configure fortune

--- fish/config.fish
+++ fish/config.fish
@@ -115,3 +115,4 @@ set -x SHELL /bin/zsh
 # asdf
 source /usr/local/opt/asdf/asdf.fish
 
+fortune -s 10% art 10% law 10% science 10% ascii-art 10% wisdom 10% startrek 10% work 10% humorists 10% fortunes 10% computers
