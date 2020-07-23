071652bf90ed886d6f1b4a473117f5bb0351fd3e
jbharat <bharatjoshi.p@gmail.com>
2020-07-24 08:33:09 +1000

    add fish functions to quick go up the folder tree. Upto 6 levels up the folder tree

---
+++ fish/custom/dots.fish
@@ -0,0 +1,19 @@
+function ... --description 'Jump 2 folder back'
+  cd ../../
+end
+
+function .... --description 'Jump 3 folder back'
+  cd ../../../
+end
+
+function ..... --description 'Jump 4 folder back'
+  cd ../../../..
+end
+
+function ...... --description 'Jump 5 folder back'
+  cd ../../../..
+end
+
+function ....... --description 'Jump 6 folder back'
+  cd ../../../..
+end
