1413add1df82266bb9eefcdc224a6eb14b2766e1
jbharat <bharatjoshi.p@gmail.com>
2020-09-08 11:39:56 +1000

    update fish prompt to show error status

--- fish/config.fish
+++ fish/config.fish
@@ -119,7 +119,11 @@ set fish_color_cwd blue
 set __fish_git_prompt_color_branch magenta
 
 function fish_prompt --description 'Write out the prompt'
-  echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (fish_vcs_prompt) (set_color normal) '> '
+  set -l prompt_color blue
+  if test $status -ne 0
+    set prompt_color red
+  end
+  echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (fish_vcs_prompt) (set_color $prompt_color) '> '
 end
 
 function fish_greeting --description 'Override fish_greeting'
