e99574a3542e0d3ce58145a94a55098cd9b2b87d
jbharat <bharatjoshi.p@gmail.com>
2020-09-08 19:22:30 +1000

    update fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -113,8 +113,15 @@ set -x SHELL /bin/zsh
 
 
 set __fish_git_prompt_show_informative_status
-set __fish_git_prompt_showcolorhints
-set __fish_git_prompt_showupstream "informative"
+set __fish_git_prompt_showcolorhints 'yes'
+set __fish_git_prompt_showupstream 'informative'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_char_cleanstate '✔'
+set __fish_git_prompt_char_dirtystate '◆'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_upstream_diverged '<>'
+set __fish_git_prompt_color_upstream cyan
 set fish_color_cwd blue
 set __fish_git_prompt_color_branch magenta
 
@@ -123,7 +130,11 @@ function fish_prompt --description 'Write out the prompt'
   if test $status -ne 0
     set prompt_color red
   end
-  echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (fish_vcs_prompt) (set_color $prompt_color) '> '
+  set -l pwd (prompt_pwd)
+  if [ $pwd = '~' ]
+    set pwd ''
+  end
+  echo -n -s (set_color $fish_color_cwd) $pwd (fish_vcs_prompt) (set_color $prompt_color) '~>> '
 end
 
 function fish_greeting --description 'Override fish_greeting'
