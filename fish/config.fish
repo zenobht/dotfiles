6b8633744151318c3bad9007d64a0eee45758b43
Bharat Joshi <bharatjoshi.p@gmail.com>
2020-09-20 18:12:10 +1000

    fix fzf not showing preview on some files

--- fish/config.fish
+++ fish/config.fish
@@ -177,7 +177,7 @@ end
 set fish_greeting
 
 function f --description 'Fuzzy find file and open in vim'
-    set files (echo (eval "$FZF_DEFAULT_COMMAND | fzf --multi --exit-0 --preview 'bat --style=plain --color=always --line-range 100: {}'"))
+    set files (echo (eval "$FZF_DEFAULT_COMMAND | fzf --multi --exit-0 --preview 'bat --style=plain --color=always --line-range :100 {}'"))
     if test -n $files
       v (echo $files | string split ' ')
     end
