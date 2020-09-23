1792f098bae582c9dd410c6b4552335726bfb6f1
jbharat <bharatjoshi.p@gmail.com>
2020-09-23 22:38:01 +1000

    delete unused files

--- start_ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# this script runs ssh with a different TERM
-# this is required as my term is custom term
-# sometimes backspace stops working on ssh'd system with this custom term
-
-TERM=xterm-256color ssh "$@"
