fec73dcfa64ab2b3918ec5cbcf8d5f59381b966e
Bharat Joshi <bharatjoshi.p@gmail.com>
2020-10-04 21:00:30 +1100

    fix bug with abbr v in fish function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -153,7 +153,7 @@ set fish_greeting
 function f --description 'Fuzzy find file and open in vim'
     set files (echo (eval "$FZF_DEFAULT_COMMAND | fzf --multi --exit-0 --preview 'bat --style=plain --color=always --line-range :100 {}'"))
     if test -n $files
-      v (echo $files | string split ' ')
+      nvim (echo $files | string split ' ')
     end
 end
 
