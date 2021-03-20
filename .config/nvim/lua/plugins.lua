bf6df3a7a4322a8275ad00f26077b8a3a1b4ce0d
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-03-20 20:24:56 +1100

    [nvim] hide close button for bufferline

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -31,6 +31,7 @@ return require('packer').startup(function()
       require('bufferline').setup{
         options = {
           show_buffer_close_icons = false,
+          show_close_icon = false,
           separator_style = "thick",
           mappings = false,
         }
