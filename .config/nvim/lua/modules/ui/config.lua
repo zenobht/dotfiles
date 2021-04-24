dbc59712d1f5b9707af8558834b2089219b81b8f
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-04-24 21:31:57 +1000

    [nvim] fix nvim-tree duplicate files/folder issue.
    refer https://github.com/kyazdani42/nvim-tree.lua/issues/342

--- .config/nvim/lua/modules/ui/config.lua
+++ .config/nvim/lua/modules/ui/config.lua
@@ -91,7 +91,9 @@ function config.tree()
       untracked = "★",
     },
   }
-  require"nvim-tree".on_enter()
+  require'nvim-tree.events'.on_nvim_tree_ready(function ()
+    vim.cmd("NvimTreeRefresh")
+  end)
 end
 
 function config.signify()
