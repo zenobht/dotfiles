767a28ca66b7cad155dabf6ce49baf8a509518ab
jbharat <bharatjoshi.p@gmail.com>
2021-04-30 11:36:59 +1000

    [nvim] hide indentline on vim help

--- .config/nvim/lua/modules/ui/config.lua
+++ .config/nvim/lua/modules/ui/config.lua
@@ -6,17 +6,18 @@ function config.indentBlankLine()
   vim.g.indent_blankline_use_treesitter = true
   -- vim.g.indent_blankline_show_current_context = true
   vim.g.indent_blankline_filetype_exclude = {
-    "startify",
-    "log",
-    "gitcommit",
-    "packer",
-    "markdown",
-    "json",
-    "txt",
+    "", -- for all buffers without a file type
     "NvimTree",
     "git",
+    "gitcommit",
+    "help",
+    "json",
+    "log",
+    "markdown",
+    "packer",
+    "startify",
+    "txt",
     "undotree",
-    "" -- for all buffers without a file type
   }
   vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
 end
