6538652dc48ea2476518f282ed3b84953e0e690f
jbharat <bharatjoshi.p@gmail.com>
2021-05-11 17:47:26 +1000

    [nvim] configure fzf-writer for faster rendering

--- .config/nvim/lua/modules/editor/config.lua
+++ .config/nvim/lua/modules/editor/config.lua
@@ -88,7 +88,9 @@ function config.telescope()
           override_generic_sorter = false, -- override the generic sorter
           override_file_sorter = true,     -- override the file sorter
           case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                          -- the default case_mode is "smart_case"
+          use_highlighter = false,
+          minimum_grep_characters = 3,
+          minimum_files_characters = 3
         }
       }
     }
