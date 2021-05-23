f14eb9dc63bbbe8874490f1308cd3ef004aaffcc
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-05-23 11:53:40 +1000

    [nvim] configure kotlin_language_server

--- .config/nvim/lua/modules/completion/config.lua
+++ .config/nvim/lua/modules/completion/config.lua
@@ -65,7 +65,7 @@ function config.lspconfig()
 
   -- Use a loop to conveniently both setup defined servers
   -- and map buffer local keybindings when the language server attaches
-  local servers = { "tsserver" }
+  local servers = { "tsserver", "kotlin_language_server" }
   for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
       on_attach = on_attach,
