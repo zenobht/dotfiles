c0e16e36d4a4a6cb61274b5aa579e784c80e38c1
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-06-20 10:23:05 +1000

    [nvim] update treesitter loading event

--- .config/nvim/lua/modules/lang/plugins.lua
+++ .config/nvim/lua/modules/lang/plugins.lua
@@ -2,7 +2,7 @@ local lang = {}
 local conf = require('modules.lang.config')
 
 lang['nvim-treesitter/nvim-treesitter'] = {
-  event = 'VimEnter',
+  event = {'BufRead', 'BufNewFile'},
   config = conf.treesitter
 }
 
