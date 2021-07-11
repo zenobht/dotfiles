0bf63c2795bfd7bfa5f5803ef97bbd06795f0b4a
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-07-11 18:40:28 +1000

    [nvim] install maintained treesitter packages

--- nvim/.config/nvim/lua/modules/lang/config.lua
+++ nvim/.config/nvim/lua/modules/lang/config.lua
@@ -4,6 +4,7 @@ function config.treesitter()
   -- vim.api.nvim_command('set foldmethod=expr')
   -- vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require('nvim-treesitter.configs').setup {
+    ensure_installed = "maintained",
     highlight = {
       enable = true
     },
