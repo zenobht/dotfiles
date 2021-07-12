76343b0378fc114f4587a865ed7dc0615e39253f
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-07-12 20:56:21 +1000

    [fish] update fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -10,11 +10,12 @@ set __fish_git_prompt_char_upstream_diverged '<>'
 set __fish_git_prompt_color_upstream cyan
 set fish_color_cwd blue
 set __fish_git_prompt_color_branch magenta
+set -U fish_prompt_pwd_dir_length 0
 
 function fish_prompt --description 'Write out the prompt'
     set -l git (fish_git_prompt)
 
-    set -l prompt ' ❯ '
+    set -l prompt '❯ '
 
     set -l prompt_color green
 
@@ -24,5 +25,5 @@ function fish_prompt --description 'Write out the prompt'
 
     set -l pwd (prompt_pwd)
 
-    echo -n -s (set_color $fish_color_cwd) $pwd $git (set_color $prompt_color) $prompt
+    echo -n -s -e (set_color $fish_color_cwd) $pwd $git (set_color $prompt_color) '\n' $prompt
 end
