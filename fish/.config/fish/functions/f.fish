88ac856206b065ec4077d3ecf4e120f98e1003b4
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-07-17 15:53:15 +1000

    [fish] remove multi for f command
    fzf does not handle separators properly for multi selection. There is no
    option to change the separators. Hence disable multi. This is because
    fzf returns file names as a string with space separated. But if the file
    itself has space, there is no way to know where to split

--- fish/.config/fish/functions/f.fish
+++ fish/.config/fish/functions/f.fish
@@ -1,6 +1,6 @@
 function f --description 'Fuzzy find file and open in vim'
-    set files (echo (eval "$FZF_DEFAULT_COMMAND | fzf --multi --exit-0 --preview 'bat --style=plain --color=always --line-range :100 {}'"))
-    if test -n $files
-      nvim (echo $files | string split ' ')
+    set file (echo (eval "$FZF_DEFAULT_COMMAND | fzf --exit-0 --preview 'bat --style=plain --color=always --line-range :100 {}'"))
+    if test -n $file
+      nvim (echo $file)
     end
 end
