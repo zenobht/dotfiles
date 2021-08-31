86b38635a352c6cf2cfff008150cbf41b57a57af
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-08-31 13:47:13 +1000

    nvim: remove deprecated mappings

--- nvim/.config/nvim/lua/modules/ui/config.lua
+++ nvim/.config/nvim/lua/modules/ui/config.lua
@@ -6,7 +6,6 @@ function config.bufferline()
       show_buffer_close_icons = false,
       show_close_icon = false,
       separator_style = "thick",
-      mappings = false,
       always_show_bufferline = false,
     },
   }
