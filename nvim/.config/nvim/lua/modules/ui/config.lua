1a6097cfa6ac919adb6f3a7b4c27aedeec3009c5
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-10-24 10:12:20 +1100

    alacritty: enable transparent background

--- nvim/.config/nvim/lua/modules/ui/config.lua
+++ nvim/.config/nvim/lua/modules/ui/config.lua
@@ -162,7 +162,8 @@ end
 
 
 function config.theme()
-
+  vim.g.tokyonight_transparent = true
+  vim.g.tokyonight_transparent_sidebar = true
   vim.g.tokyonight_style = "night"
   vim.g.tokyonight_italic_functions = true
   vim.g.tokyonight_italic_comments = true
