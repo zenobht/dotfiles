6bbb10b3e6c9cf19d02da320c6f3ab93a30ca1ed
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-10-27 20:10:32 +1100

    hammerspoon: rollout custom window management with hammerspoon
    yabai is not working with monetary. So use hammerspoon to create
    a simple window management system

---
+++ hammerspoon/.hammerspoon/windows.lua
@@ -0,0 +1,318 @@
+hs.window.animationDuration = 0
+window = hs.getObjectMetatable("hs.window")
+
+-- +-----------------+
+-- |        |        |
+-- |  HERE  |        |
+-- |        |        |
+-- +-----------------+
+function window.left(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |        |        |
+-- |        |  HERE  |
+-- |        |        |
+-- +-----------------+
+function window.right(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+function window.up(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.w = max.w
+  f.y = max.y
+  f.h = max.h / 2
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+function window.down(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.w = max.w
+  f.y = max.y + (max.h / 2)
+  f.h = max.h / 2
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |  HERE  |        |
+-- +--------+        |
+-- |                 |
+-- +-----------------+
+function window.upLeft(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:fullFrame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w/2
+  f.h = max.h/2
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |                 |
+-- +--------+        |
+-- |  HERE  |        |
+-- +-----------------+
+function window.downLeft(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:fullFrame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w/2
+  f.h = max.h/2
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |                 |
+-- |        +--------|
+-- |        |  HERE  |
+-- +-----------------+
+function window.downRight(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:fullFrame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y + (max.h / 2)
+  f.w = max.w/2
+  f.h = max.h/2
+
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |        |  HERE  |
+-- |        +--------|
+-- |                 |
+-- +-----------------+
+function window.upRight(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:fullFrame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w/2
+  f.h = max.h/2
+  win:setFrame(f)
+end
+
+-- +--------------+
+-- |  |        |  |
+-- |  |  HERE  |  |
+-- |  |        |  |
+-- +---------------+
+function window.centerWithFullHeight(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:fullFrame()
+
+  f.x = max.x + (max.w / 5)
+  f.w = max.w * 3/5
+  f.y = max.y
+  f.h = max.h
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |      |          |
+-- | HERE |          |
+-- |      |          |
+-- +-----------------+
+function window.left40(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w * 0.4
+  f.h = max.h
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |      |          |
+-- |      |   HERE   |
+-- |      |          |
+-- +-----------------+
+function window.right60(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w * 0.4)
+  f.y = max.y
+  f.w = max.w * 0.6
+  f.h = max.h
+  win:setFrame(f)
+end
+
+function window.focus_left(w)
+  hs.window.focusedWindow():focusWindowWest()
+end
+
+function window.focus_right(w)
+  hs.window.focusedWindow():focusWindowEast()
+end
+
+function window.focus_north(w)
+  hs.window.focusedWindow():focusWindowNorth()
+end
+
+function window.focus_south(w)
+  hs.window.focusedWindow():focusWindowSouth()
+end
+
+function window.nextScreen(win)
+  local currentScreen = win:screen()
+  local allScreens = hs.screen.allScreens()
+  currentScreenIndex = hs.fnutils.indexOf(allScreens, currentScreen)
+  nextScreenIndex = currentScreenIndex + 1
+
+  if allScreens[nextScreenIndex] then
+    win:moveToScreen(allScreens[nextScreenIndex])
+  else
+    win:moveToScreen(allScreens[1])
+  end
+end
+
+windowLayoutMode = hs.hotkey.modal.new({}, 'F16')
+
+windowLayoutMode.entered = function()
+  windowLayoutMode.statusMessage:show()
+end
+windowLayoutMode.exited = function()
+  windowLayoutMode.statusMessage:hide()
+end
+
+-- Bind the given key to call the given function and exit WindowLayout mode
+function windowLayoutMode.bindWithAutomaticExit(mode, modifiers, key, fn)
+  mode:bind(modifiers, key, function()
+    mode:exit()
+    fn()
+  end)
+end
+
+local status, windowMappings = pcall(require, 'windows-bindings')
+
+if not status then
+  windowMappings = require('windows-bindings-defaults')
+end
+
+local modifiers = windowMappings.modifiers
+local showHelp  = windowMappings.showHelp
+local trigger   = windowMappings.trigger
+local mappings  = windowMappings.mappings
+
+function getModifiersStr(modifiers)
+  local modMap = { shift = '⇧', ctrl = '⌃', alt = '⌥', cmd = '⌘' }
+  local retVal = ''
+
+  for i, v in ipairs(modifiers) do
+    retVal = retVal .. modMap[v]
+  end
+
+  return retVal
+end
+
+local msgStr = getModifiersStr(modifiers)
+msgStr = 'Window Layout Mode (' .. msgStr .. (string.len(msgStr) > 0 and '+' or '') .. trigger .. ')'
+
+for i, mapping in ipairs(mappings) do
+  local modifiers, trigger, winFunction = table.unpack(mapping)
+  local hotKeyStr = getModifiersStr(modifiers)
+
+  if showHelp == true then
+    if string.len(hotKeyStr) > 0 then
+      msgStr = msgStr .. (string.format('\n%10s+%s => %s', hotKeyStr, trigger, winFunction))
+    else
+      msgStr = msgStr .. (string.format('\n%11s => %s', trigger, winFunction))
+    end
+  end
+
+  windowLayoutMode:bindWithAutomaticExit(modifiers, trigger, function()
+    --example: hs.window.focusedWindow():upRight()
+    local fw = hs.window.focusedWindow()
+    fw[winFunction](fw)
+  end)
+end
+
+local message = require('status-message')
+windowLayoutMode.statusMessage = message.new(msgStr)
+
+-- Use modifiers+trigger to toggle WindowLayout Mode
+hs.hotkey.bind(modifiers, trigger, function()
+  windowLayoutMode:enter()
+end)
+windowLayoutMode:bind(modifiers, trigger, function()
+  windowLayoutMode:exit()
+end)
+
+global_border = nil
+
+function redrawBorder()
+    win = hs.window.focusedWindow()
+    if win ~= nil then
+        top_left = win:topLeft()
+        size = win:size()
+        if global_border ~= nil then
+            global_border:delete()
+        end
+        global_border = hs.drawing.rectangle(hs.geometry.rect(top_left['x'], top_left['y'], size['w'], size['h']))
+        global_border:setStrokeColor({["red"]=0.4,["blue"]=0.9,["green"]=0.3,["alpha"]=0.2})
+        global_border:setFill(false)
+        global_border:setStrokeWidth(6)
+        global_border:show()
+    end
+end
+
+redrawBorder()
+
+allwindows = hs.window.filter.new(nil)
+allwindows:subscribe(hs.window.filter.windowCreated, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowFocused, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowMoved, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowUnfocused, function () redrawBorder() end)
+
