dd66973f3de1161855de863e303854d5cc6713ea
zenobharat <bharatjoshi.p@gmail.com>
2021-11-09 12:59:30 +1100

    nvim: add bindings to jump to hunks in a buffer

--- nvim/.config/nvim/lua/modules/ui/config.lua
+++ nvim/.config/nvim/lua/modules/ui/config.lua
@@ -12,9 +12,6 @@ function config.gitsigns()
       noremap = true,
       buffer = true,
 
-      -- ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-      -- ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
-
       -- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
       -- ['n <leader>hu'] = '<cmd>lua requiregitsigns".undo_stage_hunk()<CR>',
       -- ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
