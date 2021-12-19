0758e96c7347a4cbbef6c542f33d3dc702e3aeab
Bharat Joshi <bharatjoshi.p@gmail.com>
2021-12-19 21:11:44 +1100

    nvim: add nnn mapping

--- nvim/.config/nvim/lua/modules/ui.lua
+++ nvim/.config/nvim/lua/modules/ui.lua
@@ -3,7 +3,18 @@ local plugins = {}
 plugins["luukvbaal/nnn.nvim"] = {
   cmd = 'NnnPicker',
   config = function ()
-    require("nnn").setup()
+    local builtin = require("nnn").builtin
+    require("nnn").setup({
+      mappings = {
+        { "<C-t>", builtin.open_in_tab },       -- open file(s) in tab
+        { "<C-s>", builtin.open_in_split },     -- open file(s) in split
+        { "<C-v>", builtin.open_in_vsplit },    -- open file(s) in vertical split
+        { "<C-p>", builtin.open_in_preview },   -- open file in preview split keeping nnn focused
+        { "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
+        { "<C-w>", builtin.cd_to_path },        -- cd to file directory
+        { "<C-e>", builtin.populate_cmdline },  -- populate cmdline (:) with file(s)
+      }
+    })
   end
 }
 
