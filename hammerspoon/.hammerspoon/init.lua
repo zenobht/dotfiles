bd5fbf4b260e0789825247528c01d515296a0730
zenobht <bharatjoshi.p@gmail.com>
2022-01-13 11:06:09 +1100

    kitty: update kitty keymaps

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,7 +1,7 @@
 local log = hs.logger.new('init.lua', 'debug')
 
 -- Use Control+` to reload Hammerspoon config
-hs.hotkey.bind({'alt','ctrl'}, 'h', nil, function()
+hs.hotkey.bind({'alt','ctrl'}, '-', nil, function()
   hs.reload()
 end)
 
