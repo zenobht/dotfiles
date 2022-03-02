95b36f49be1ec59e56a0ae7d4fa8597bd6e814b4
zenobht <bharatjoshi.p@gmail.com>
2022-03-02 11:48:23 +1100

    nvim: use new autocmd api of nvim to create events

--- nvim/.config/nvim/lua/core/event.lua
+++ nvim/.config/nvim/lua/core/event.lua
@@ -1,15 +1,17 @@
-local vim = vim
+local create_autocmd = vim.api.nvim_create_autocmd
+local create_autogroup = vim.api.nvim_create_augroup
 local autocmd = {}
 
 function autocmd.nvim_create_augroups(definitions)
   for group_name, definition in pairs(definitions) do
-    vim.api.nvim_command('augroup '..group_name)
-    vim.api.nvim_command('autocmd!')
+    create_autogroup(group_name, {clear = true})
     for _, def in ipairs(definition) do
-      local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
-      vim.api.nvim_command(command)
+      create_autocmd(def[1], {
+        group = group_name,
+        pattern = def[2],
+        command = def[3]
+      })
     end
-    vim.api.nvim_command('augroup END')
   end
 end
 
@@ -43,7 +45,7 @@ function autocmd.load_autocmds()
     };
 
     yank = {
-      {"TextYankPost", [[* silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=400})]]};
+      {"TextYankPost", "*", "silent! lua vim.highlight.on_yank({higroup='IncSearch', timeout=400})"};
     };
   }
 
