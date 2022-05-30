fb843fca52b1e0f254c4f0f4130ceae830647c13
zenobht <bharatjoshi.p@gmail.com>
2022-05-30 17:48:55 +1000

    nvim: fix lsp issues

--- nvim/.config/nvim/lua/modules/completion.lua
+++ nvim/.config/nvim/lua/modules/completion.lua
@@ -16,8 +16,7 @@ plugins["neovim/nvim-lspconfig"] = {
 }
 
 plugins["williamboman/nvim-lsp-installer"] = {
-  event = "VimEnter",
-  requires = {'neovim/nvim-lspconfig', 'hrsh7th/nvim-cmp'},
+  requires = {'neovim/nvim-lspconfig'},
   config = function ()
 
     local on_attach = function(client, bufnr)
@@ -39,10 +38,10 @@ plugins["williamboman/nvim-lsp-installer"] = {
       buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
       buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
       buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-      buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.open_float()<CR>', opts)
-      buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-      buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-      buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.setlocList()<CR>', opts)
+      buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+      buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+      buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+      buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setlocList()<CR>', opts)
 
       -- Set some keybinds conditional on server capabilities
       if client.resolved_capabilities.document_formatting then
@@ -74,7 +73,7 @@ plugins["williamboman/nvim-lsp-installer"] = {
     local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-    local servers = { "dockerls", "html", "jdtls", "jsonls", "kotlin_language_server", "pyright", "sqls", "tsserver", "yamlls", "jsonls" }
+    local servers = { "dockerls", "html", "jdtls", "jsonls", "kotlin_language_server", "pyright", "sqls", "tsserver", "yamlls"}
     require("nvim-lsp-installer").setup {
        automatic_installation = true,
     }
@@ -86,6 +85,8 @@ plugins["williamboman/nvim-lsp-installer"] = {
       }
     end
     lspconfig.sumneko_lua.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
       settings = {
         Lua = {
           diagnostics = {
@@ -124,6 +125,8 @@ plugins["williamboman/nvim-lsp-installer"] = {
       }
     }
     lspconfig.diagnosticls.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
       filetypes = {
         "javascript",
         "javascript.jsx",
