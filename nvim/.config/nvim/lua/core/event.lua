f310af30c4cad45f12523311e5ba5a6391407748
bharat joshi <bharat@Zeno-2.local>
2022-06-26 14:11:33 +1000

    nvim: add randomizer for alpha header.
    remove cursorline event since it enables cursorline for alpha which is
    not required. Until a function to handle this is created, commenting out
    this event

--- nvim/.config/nvim/lua/core/event.lua
+++ nvim/.config/nvim/lua/core/event.lua
@@ -43,10 +43,10 @@ function autocmd.load_autocmds()
       {"FileType", "markdown", "set conceallevel=0"},
     };
 
-    cursor = {
-      {"InsertEnter,WinLeave", "*", "set nocursorline"},
-      {"InsertLeave,WinEnter", "*", "set cursorline"},
-    };
+    -- cursor = {
+    --   {"InsertEnter,WinLeave", "*", "set nocursorline"},
+    --   {"InsertLeave,WinEnter", "*", "set cursorline"},
+    -- };
 
     term = {
       {"TermOpen", "*", "lua require('utils').onTermOpen()"},
