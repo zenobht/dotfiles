807c9c0219f646514d7701cbfbc06a47433e8c2c
bharat joshi <bharat@Zeno-2.local>
2022-11-27 20:03:09 +1100

    nvim: update nvim-scrollbar config

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -439,9 +439,12 @@ plugin {
       show = true,
       set_highlights = true,
       handlers = {
+        cursor = true,
         diagnostic = true,
-        search = true, -- Requires hlslens to be loaded, will run require("scrollbar.handlers.search").setup() for you
-      },
+        gitsigns = false, -- Requires gitsigns
+        handle = true,
+        search = false, -- Requires hlslens,
+      }
     })
     require("scrollbar.handlers.search").setup()
   end
