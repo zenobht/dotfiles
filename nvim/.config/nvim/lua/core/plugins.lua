3718858557a33a60ca713b3a9e73129796d84e58
Bharat Joshi <bharat@Bharats-MacBook-Pro.local>
2024-03-09 11:57:53 +1100

    nvim: update telescope config

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -352,7 +352,7 @@ require("lazy").setup({
 
   {
     'nvim-telescope/telescope-fzf-native.nvim',
-    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    build = "make",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
     config = function ()
       require('telescope').load_extension('fzf')
