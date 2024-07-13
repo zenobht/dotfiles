7269303c845dc5dcfb77aee797580942e96082e2
zenobht <bharatjoshi.p@gmail.com>
2024-07-13 21:38:05 +1000

    nvim: update which-key config as per new spec

--- nvim/.config/nvim/lua/keymap/init.lua
+++ nvim/.config/nvim/lua/keymap/init.lua
@@ -80,112 +80,96 @@ set({'i','s'}, "<C-y>", "v:lua.vsnip_expand()", { expr = true })
 
 local wk = require("which-key")
 
-wk.register({
-  b = {
-    name = "buffer",
-    b = { map_lua("require('telescope.builtin').buffers()"), "list buffers" },
-    c = { map_cr("nohl"), "Clear search" },
-    d = { map_cr("bd"), "delete buffer" },
-    D = { map_cr("bd!"), "Force delete buffer" },
-    -- l = { map_args("ls<CR>:b"), "list buffers"},
-    l = { "<cmd>HopLine<CR>", "Hop Line"},
-    o = { map_cr("b#"), "alternate buffer"},
-    s = { map_lua("require('telescope.builtin').current_buffer_fuzzy_find()"), "Search in buffer"},
-    t = { map_lua("require('Trailspace').trim()"), "Trim trailing whitespace" },
-    ['-'] = { map_cr("Scratch"), "Scratch buffer" },
-  },
-  d = {
-    name = "dotfiles",
-    f = { map_cr("Dff"), "Find file in dotfiles" },
-    k = { map_cr("Dfc"), "close dotfile buffers" },
-    r = { map_cr("Reload"), "Reload nvim" },
-    s = { map_cr("Dfs"), "Search in dotfiles" },
-    w = { map_cr("Dfg"), "Search word in dotfiles" },
-  },
-  f = {
-    name = "file",
-    f = { map_lua("require('telescope.builtin').find_files({hidden=true})"), "Find file"},
-    n = { map_lua("require('keymap.utils').nnnPicker()"), "Open file picker"},
-    p = { map_cr("MarkdownPreviewToggle"), "Preview" },
-    s = { map_lua("require('telescope.builtin').live_grep()"), "Search in file"},
-    t = { map_cr("NvimTreeToggle"), "Open file tree"},
-    w = { map_lua("require('telescope.builtin').grep_string()"), "Search string"},
-    y = { map_cr("let @+ = expand('%')"), "Yank current file path" }, -- yank current file path
-  },
-  g = {
-    name = "git",
-    b = { map_lua("require('telescope.builtin').git_branches()"), "Branches" },
-    c = { map_lua("require('telescope.builtin').git_commits()"), "Commits" },
-    C = { map_lua("require('keymap.custom').git_conflicts({})"), "Conflict files" },
-    f = { map_lua("require('keymap.utils').openTerm('tig ' .. vim.fn.expand('%'))"), "Tig file history" },
-    g = { map_lua("require('neogit').open()"), "Neogit" },
-    l = { map_lua("require('gitsigns').blame_line()"), "Blame line" },
-    L = { map_lua("require('keymap.utils').openTerm('tig')"), "Tig log" },
-    t = { map_lua("require('keymap.utils').openTerm('tig status')"), "Tig status" },
-  },
-  l = {
-    name = "Lsp",
-    i = { map_cr("LspInfo"), "Lsp info"},
-    k = { map_cr("LspStop"), "Kill lsp"},
-    r = { map_cr("LspRestart"), "Restart lsp"},
-    s = { map_cr("LspStart"), "Start lsp"},
-  },
-  o = {
-    name = "others",
-    n = { map_lua("require('keymap.utils').toggleNumbers()"), "Cycle number" },
-  },
-  s = {
-    name = "session",
-    d = { map_wait("SD " .. require('keymap.utils').getSessionFilePath()), "Delete session" },
-    r = { map_wait("SR " .. require('keymap.utils').getSessionFilePath()), "Reload session" },
-    s = { map_args("SS"), "Save session" },
-  },
-  z = {
-    name = "lazy",
-    c = { map_lua("require('lazy').health()"), "Check health" },
-    h = { map_lua("require('lazy').home()"), "Home" },
-    p = { map_lua("require('lazy').profile()"), "Profile" },
-    s = { map_lua("require('lazy').sync()"), "Sync" },
-    z = { map_lua("require('lazy').sync()"), "Sync" },
-  }
-}, { prefix = "<leader>" })
+wk.add({
+  { "<leader>b", group = "buffer" },
+  { "<leader>b-", ":Scratch<CR>", desc = "Scratch buffer" },
+  { "<leader>bD", ":bd!<CR>", desc = "Force delete buffer" },
+  { "<leader>bb", "<cmd>lua require('telescope.builtin').buffers()<CR>", desc = "list buffers" },
+  { "<leader>bc", ":nohl<CR>", desc = "Clear search" },
+  { "<leader>bd", ":bd<CR>", desc = "delete buffer" },
+  { "<leader>bl", "<cmd>HopLine<CR>", desc = "Hop Line" },
+  { "<leader>bo", ":b#<CR>", desc = "alternate buffer" },
+  { "<leader>bs", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", desc = "Search in buffer" },
+  { "<leader>bt", "<cmd>lua require('Trailspace').trim()<CR>", desc = "Trim trailing whitespace" },
+  { "<leader>d", group = "dotfiles" },
+  { "<leader>df", ":Dff<CR>", desc = "Find file in dotfiles" },
+  { "<leader>dk", ":Dfc<CR>", desc = "close dotfile buffers" },
+  { "<leader>dr", ":Reload<CR>", desc = "Reload nvim" },
+  { "<leader>ds", ":Dfs<CR>", desc = "Search in dotfiles" },
+  { "<leader>dw", ":Dfg<CR>", desc = "Search word in dotfiles" },
+  { "<leader>f", group = "file" },
+  { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files({hidden=true})<CR>", desc = "Find file" },
+  { "<leader>fn", "<cmd>lua require('keymap.utils').nnnPicker()<CR>", desc = "Open file picker" },
+  { "<leader>fp", ":MarkdownPreviewToggle<CR>", desc = "Preview" },
+  { "<leader>fs", "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = "Search in file" },
+  { "<leader>ft", ":NvimTreeToggle<CR>", desc = "Open file tree" },
+  { "<leader>fw", "<cmd>lua require('telescope.builtin').grep_string()<CR>", desc = "Search string" },
+  { "<leader>fy", ":let @+ = expand('%')<CR>", desc = "Yank current file path" },
+  { "<leader>g", group = "git" },
+  { "<leader>gC", "<cmd>lua require('keymap.custom').git_conflicts({})<CR>", desc = "Conflict files" },
+  { "<leader>gL", "<cmd>lua require('keymap.utils').openTerm('tig')<CR>", desc = "Tig log" },
+  { "<leader>gb", "<cmd>lua require('telescope.builtin').git_branches()<CR>", desc = "Branches" },
+  { "<leader>gc", "<cmd>lua require('telescope.builtin').git_commits()<CR>", desc = "Commits" },
+  { "<leader>gf", "<cmd>lua require('keymap.utils').openTerm('tig ' .. vim.fn.expand('%'))<CR>", desc = "Tig file history" },
+  { "<leader>gg", "<cmd>lua require('neogit').open()<CR>", desc = "Neogit" },
+  { "<leader>gl", "<cmd>lua require('gitsigns').blame_line()<CR>", desc = "Blame line" },
+  { "<leader>gt", "<cmd>lua require('keymap.utils').openTerm('tig status')<CR>", desc = "Tig status" },
+  { "<leader>l", group = "Lsp" },
+  { "<leader>li", ":LspInfo<CR>", desc = "Lsp info" },
+  { "<leader>lk", ":LspStop<CR>", desc = "Kill lsp" },
+  { "<leader>lr", ":LspRestart<CR>", desc = "Restart lsp" },
+  { "<leader>ls", ":LspStart<CR>", desc = "Start lsp" },
+  { "<leader>o", group = "others" },
+  { "<leader>on", "<cmd>lua require('keymap.utils').toggleNumbers()<CR>", desc = "Cycle number" },
+  { "<leader>s", group = "session" },
+  { "<leader>sd", ":SD ~/.vim/sessions/_Users_bharat_.dotfiles-", desc = "Delete session" },
+  { "<leader>sr", ":SR ~/.vim/sessions/_Users_bharat_.dotfiles-", desc = "Reload session" },
+  { "<leader>ss", ":SS<Space>", desc = "Save session" },
+  { "<leader>z", group = "lazy" },
+  { "<leader>zc", "<cmd>lua require('lazy').health()<CR>", desc = "Check health" },
+  { "<leader>zh", "<cmd>lua require('lazy').home()<CR>", desc = "Home" },
+  { "<leader>zp", "<cmd>lua require('lazy').profile()<CR>", desc = "Profile" },
+  { "<leader>zs", "<cmd>lua require('lazy').sync()<CR>", desc = "Sync" },
+  { "<leader>zz", "<cmd>lua require('lazy').sync()<CR>", desc = "Sync" },
+})
 
-wk.register({
-  f = {
-    name = "file",
-    w = { map_lua("require('telescope.builtin').grep_string({search = require('keymap.utils').getVisualSelection()})"), "Search visual selection"},
+wk.add({
+  { "<leader>f", group = "file", mode = "v" },
+  { "<leader>fw", "<cmd>lua require('telescope.builtin').grep_string({search = require('keymap.utils').getVisualSelection()})<CR>", desc = "Search visual selection", mode = "v" },
+})
+
+wk.add({
+  { "!", "f cl<CR><ESC>l", desc = "Split lines" },
+  { "#", "#<Cmd>lua require('hlslens').start()<CR>", desc = "Search word under cursor" },
+  { "*", "*<Cmd>lua require('hlslens').start()<CR>", desc = "Search word under cursor" },
+  { "<C-S-j>", ":m .+1<CR>==", desc = "Move down line" },
+  { "<C-S-k>", ":m .-2<CR>==", desc = "Move up line" },
+  { "<C-h>", ":BufferLineCyclePrev<CR>", desc = "Previous buffer" },
+  { "<C-l>", ":BufferLineCycleNext<CR>", desc = "Next buffer" },
+  { "<M-o>", ":wincmd w<CR>", desc = "Other window" },
+  { "F", "<cmd>lua require('keymap.utils').custom_F()<CR>", desc = "F" },
+  { "N", "<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>", desc = "Previous" },
+  { "T", "<cmd>lua require('keymap.utils').custom_T()<CR>", desc = "T" },
+  { "f", "<cmd>lua require('keymap.utils').custom_f()<CR>", desc = "f" },
+  { "g#", "g#<Cmd>lua require('hlslens').start()<CR>", desc = "Search word under cursor" },
+  { "g*", "g*<Cmd>lua require('hlslens').start()<CR>", desc = "Search word under cursor" },
+  { "n", "<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>", desc = "Next" },
+  { "s", "<cmd>lua require('hop').hint_words()<CR>", desc = "Hop Word" },
+  { "t", "<cmd>lua require('keymap.utils').custom_t()<CR>", desc = "t" },
+})
+
+wk.add({
+  {
+    mode = { "v" },
+    { "<C-S-j>", ":m '>+1<CR>gv=gv", desc = "Move down selection" },
+    { "<C-S-k>", ":m '<-2<CR>gv=gv", desc = "Move up selection" },
+    { "F", "<cmd>lua require('keymap.utils').custom_F()<CR>", desc = "F" },
+    { "T", "<cmd>lua require('keymap.utils').custom_T()<CR>", desc = "T" },
+    { "f", "<cmd>lua require('keymap.utils').custom_f()<CR>", desc = "f" },
+    { "s", "<cmd>lua require('hop').hint_words()<CR>", desc = "Hop Word" },
+    { "t", "<cmd>lua require('keymap.utils').custom_t()<CR>", desc = "t" },
   },
-}, { mode = "v", prefix = "<leader>" })
-
-wk.register({
-  ["<M-o>"] = { map_cr("wincmd w"), "Other window" },
-  ["<C-h>"] = { map_cr("BufferLineCyclePrev"), "Previous buffer" },
-  ["<C-l>"] = { map_cr("BufferLineCycleNext"), "Next buffer" },
-  ["!"] = { map_cmd("f cl<CR><ESC>l"), "Split lines" },
-  ["<C-S-j>"] = { ":m .+1<CR>==", "Move down line" },
-  ["<C-S-k>"] = { ":m .-2<CR>==", "Move up line" },
-  ["f"] = { "<cmd>lua require('keymap.utils').custom_f()<CR>", "f"},
-  ["F"] = { "<cmd>lua require('keymap.utils').custom_F()<CR>", "F"},
-  ["t"] = { "<cmd>lua require('keymap.utils').custom_t()<CR>", "t"},
-  ["T"] = { "<cmd>lua require('keymap.utils').custom_T()<CR>", "T"},
-  ["s"] = { "<cmd>lua require('hop').hint_words()<CR>", "Hop Word"},
-  n = { [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], "Next" },
-  N = { [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], "Previous" },
-  ["*"] = { [[*<Cmd>lua require('hlslens').start()<CR>]], "Search word under cursor" },
-  ["#"] = { [[#<Cmd>lua require('hlslens').start()<CR>]], "Search word under cursor" },
-  ["g*"] = { [[g*<Cmd>lua require('hlslens').start()<CR>]], "Search word under cursor" },
-  ["g#"] = { [[g#<Cmd>lua require('hlslens').start()<CR>]], "Search word under cursor" },
-}, { mode = "n" })
-
-wk.register({
-  ["<C-S-j>"] = { ":m '>+1<CR>gv=gv", "Move down selection" },
-  ["<C-S-k>"] = { ":m '<-2<CR>gv=gv", "Move up selection" },
-  ["f"] = { "<cmd>lua require('keymap.utils').custom_f()<CR>", "f"},
-  ["F"] = { "<cmd>lua require('keymap.utils').custom_F()<CR>", "F"},
-  ["t"] = { "<cmd>lua require('keymap.utils').custom_t()<CR>", "t"},
-  ["T"] = { "<cmd>lua require('keymap.utils').custom_T()<CR>", "T"},
-  ["s"] = { "<cmd>lua require('hop').hint_words()<CR>", "Hop Word"},
-}, { mode = "v" })
+})
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
