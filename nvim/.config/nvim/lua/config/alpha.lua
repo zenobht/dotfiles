1f6ad99965bf7d75564f4dd32c6893397d52067c
zenobht <bharatjoshi.p@gmail.com>
2024-07-15 00:09:20 +1000

    yazi: update yazi config

--- nvim/.config/nvim/lua/config/alpha.lua
+++ nvim/.config/nvim/lua/config/alpha.lua
@@ -205,7 +205,7 @@ local buttons = {
     button("e", " New file", ":Scratch<CR>", nil),
     button("SPC f f", " Find file", ":FzfLua files<CR>", nil),
     button("SPC f s", " Search word", ":FzfLua grep search=''<CR>", nil),
-    button("SPC f n", " Directory", ":NnnPicker<CR>", nil),
+    button("SPC f n", " Directory", "<cmd>lua require('yazi').yazi()<CR>", nil),
     button("SPC f t", "󰙅 Tree", ":NvimTreeToggle<CR>", nil),
     button("SPC h f", " Dotfiles", ":Dff<CR>", nil),
     button("SPC h s", " Dotfiles Search", ":Dfs<CR>", nil),
