b84cd890059d42506c948e392948fb3cd3f1d27f
zenobht <bharatjoshi.p@gmail.com>
2024-08-04 11:47:21 +1000

    add editorconfig. Format few files

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -1,23 +1,23 @@
 local vim = vim
-local home    = os.getenv("HOME")
-local cache_dir  = home .. '/.cache/nvim/'
+local home = os.getenv("HOME")
+local cache_dir = home .. "/.cache/nvim/"
 
 math.randomseed(os.time())
 
 -- Create cache dir and subs dir
-local createdir = function ()
+local createdir = function()
   local data_dir = {
-    cache_dir..'backup',
-    cache_dir..'session',
-    cache_dir..'swap',
-    cache_dir..'tags',
-    cache_dir..'undo'
+    cache_dir .. "backup",
+    cache_dir .. "session",
+    cache_dir .. "swap",
+    cache_dir .. "tags",
+    cache_dir .. "undo",
   }
   -- There only check once that If cache_dir exists
   -- Then I don't want to check subs dir exists
   if vim.fn.isdirectory(cache_dir) == 0 then
     os.execute("mkdir -p " .. cache_dir)
-    for _,v in pairs(data_dir) do
+    for _, v in pairs(data_dir) do
       if vim.fn.isdirectory(v) == 0 then
         os.execute("mkdir -p " .. v)
       end
@@ -28,29 +28,28 @@ end
 createdir()
 
 --disable_distribution_plugins
-vim.g.loaded_gzip              = 1
-vim.g.loaded_tar               = 1
-vim.g.loaded_tarPlugin         = 1
-vim.g.loaded_zip               = 1
-vim.g.loaded_zipPlugin         = 1
-vim.g.loaded_getscript         = 1
-vim.g.loaded_getscriptPlugin   = 1
-vim.g.loaded_vimball           = 1
-vim.g.loaded_vimballPlugin     = 1
-vim.g.loaded_matchit           = 1
-vim.g.loaded_matchparen        = 1
-vim.g.loaded_2html_plugin      = 1
-vim.g.loaded_logiPat           = 1
-vim.g.loaded_rrhelper          = 1
-vim.g.loaded_netrw             = 1
-vim.g.loaded_netrwPlugin       = 1
-vim.g.loaded_netrwSettings     = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
 -- vim.g.did_load_filetypes = 0
 -- vim.g.do_filetype_lua = 0
 
-require('core.lazy')
-require('core.options')
-require('keymap')
-require('core.event')
-
+require("core.lazy")
+require("core.options")
+require("keymap")
+require("core.event")
