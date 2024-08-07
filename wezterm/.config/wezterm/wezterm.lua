c4ad86c171bd7f5d864010255f8ec3cbfae2669f
zenobht <bharatjoshi.p@gmail.com>
2024-08-07 22:39:48 +1000

    wezterm: update wezterm key binding

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,17 +1,111 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
+
 local config = {}
 
-config.color_scheme = 'Tokyo Night'
--- config.font = wezterm.font('MonoLisa Nerd Font', { stretch = 'Expanded', weight = 'Regular' })
--- config.font = wezterm.font('MonoLisa Nerd Font', { stretch = 'Expanded', weight = 'Regular' })
+config.color_scheme = "Tokyo Night"
 config.font = wezterm.font_with_fallback({
-  { family = "MonoLisa Nerd Font",       scale = 1.1, weight = "Regular", },
-  { family = "FantasqueSansM Nerd Font", scale = 1.1, },
+  { family = "MonoLisa Nerd Font", scale = 1.1, weight = "Regular" },
+  { family = "FantasqueSansM Nerd Font", scale = 1.1 },
 })
 config.font_size = 14
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 
 config.front_end = "WebGpu"
 config.max_fps = 144
+config.scrollback_lines = 3000
+config.default_workspace = "main"
+
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.5,
+}
+
+config.leader = { key = "b", mods = "CMD", timeout_milliseconds = 1000 }
+
+config.keys = {
+  { key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  {
+    key = "p",
+    mods = "LEADER",
+    action = act.PaneSelect({
+      mode = "Activate",
+      alphabet = "123456789",
+    }),
+  },
+  { key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+  { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+  { key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+  {
+    key = "r",
+    mods = "LEADER",
+    action = act.ActivateKeyTable({
+      name = "resize_pane",
+      one_shot = false,
+    }),
+  },
+  {
+    key = "e",
+    mods = "LEADER",
+    action = act.PromptInputLine({
+      description = wezterm.format({
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      }),
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    }),
+  },
+  { key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+}
+
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1),
+  })
+end
+
+config.key_tables = {
+  -- Defines the keys that are active in our resize-pane mode.
+  -- Since we're likely to want to make multiple adjustments,
+  -- we made the activation one_shot=false. We therefore need
+  -- to define a key assignment for getting out of this mode.
+  -- 'resize_pane' here corresponds to the name="resize_pane" in
+  -- the key assignments above.
+  resize_pane = {
+    { key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+    { key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+    { key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+    { key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+    -- Cancel the mode by pressing escape
+    { key = "Escape", action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h", action = act.MoveTabRelative(-1) },
+    { key = "j", action = act.MoveTabRelative(-1) },
+    { key = "k", action = act.MoveTabRelative(1) },
+    { key = "l", action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+  },
+}
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
+
+config.window_padding = {
+  left = "1px",
+  right = "1px",
+  top = "0px",
+  bottom = "0px",
+}
 
 return config
