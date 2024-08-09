eecde5bd831c2a3c75655b45ad56caf09cbbd7e8
zenobht <bharatjoshi.p@gmail.com>
2024-08-09 16:02:38 +1000

    fish: rebind fg

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
     fzf_key_bindings
     fish_default_key_bindings
-    bind \cf 'fg 2>/dev/null; commandline -f repaint'
+    bind \cz 'fg 2>/dev/null; commandline -f repaint'
 end
